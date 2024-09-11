98e67ac11054ab825724c2ec770410b11bd1550f
karlo <kaes_21@alu.uabcs.mx>
2024-09-10 23:09:54 -0700

    trabajo 5 (trabajo en app.vue)

---
+++ 3ra_unidad/trabajo_5/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from "vue";
+const name = ref('')
+const count = ref(0)
+const num1 = ref(0)
+const num2 = ref(0)
+const result = ref(0)
+</script>
+
+<template>
+
+  <h1>nombre: {{ name }}</h1>
+  
+  <br>
+
+  <input v-model="name">
+  
+  <br>
+
+  <button @click="count++">Count is: {{ count }}</button>
+
+  <br>
+
+  <form>
+    <input v-model="num1" type="number">
+    <br>
+    <input v-model="num2" type="number">
+  </form>
+
+
+  <button @click="result = parseInt(num1) + parseInt(num2)">suma</button>
+
+  <h2>El resultado es: {{ result }}</h2>
+
+</template>
