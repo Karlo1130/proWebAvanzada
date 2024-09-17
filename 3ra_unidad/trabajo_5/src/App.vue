3946c5042704e89c190557e7bf82955718dbc016
Karlo1130 <90065654+Karlo1130@users.noreply.github.com>
2024-09-17 20:30:03 +0200

    al parecer no es un json

--- 3ra_unidad/trabajo_5/src/App.vue
+++ 3ra_unidad/trabajo_5/src/App.vue
@@ -1,12 +1,8 @@
 <script setup>
 import { ref } from "vue";
-const name = ref('')
-const count = ref(0)
-const num1 = ref(0)
-const num2 = ref(0)
-const result = ref(0)
-const newUserName = ref('')
-const newUserJob = ref('')
+
+const email = ref('')
+const password = ref('')
 
 </script>
 
@@ -27,59 +23,46 @@ export default {
     }
   },
   methods: {
-    addUsers(name, job) {
-      this.users.push(
-        {
-          name: name,
-          job: job
-        }
-      )
-      console.log({
-          name: name,
-          job: job
-        })
-    }
+
+    async onSubmit(email, password) {
+      console.log(email);
+      console.log(password);
+      console.log("hola");
+
+      // const response = await fetch("./assets/users.json");
+
+      // const json = await response.json();
+      // console.log(json);
+
+      const response = await fetch("users.json")
+      console.log(response);
+      
+      const json = await response.json();
+      console.log(json);
+    },
   },
 }
 </script>
 
 <template>
 
-  <h1>nombre: {{ name }}</h1>
+  <form @subit.prevent=onSubmit>
+    <fieldset>
+      <label>
+        Correo:
+      </label>
+      <input v-model="email" type="email" placeholder="Correo" required name="">
+    </fieldset>
+
+    <fieldset>
+      <label>
+        Contraseña:
+      </label>
+      <input v-model="password" type="password" placeholder="Contraseña" required name="">
+    </fieldset>
   
-  <br>
-
-  <input v-model="name">
-  
-  <br>
-
-  <button @click="count++">Count is: {{ count }}</button>
-
-  <br>
-
-  <form>
-    <input v-model="num1" type="number">
-    <br>
-    <input v-model="num2" type="number">
+    <button @click="onSubmit(email, password)">Acceder</button>
   </form>
 
-
-  <button @click="result = parseInt(num1) + parseInt(num2)">suma</button>
-
-  <h2>El resultado es: {{ result }}</h2>
-
-  <br>
-
-  <li v-for="user in users">
-    Nombre: {{ user.name }} - Trabajo: {{ user.job }}
-  </li>
-
-  <form>
-    <input v-model="newUserName">
-    <br>
-    <input v-model="newUserJob">
-  </form>
-
-  <button @click="addUsers(newUserName, newUserJob)">agregar nuevo usuario</button> 
 
 </template>
