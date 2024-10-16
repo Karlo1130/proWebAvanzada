1b95102d66490b2adf22b5b1b94aed0be5718a65
karlo <kaes_21@alu.uabcs.mx>
2024-10-15 21:58:36 -0700

    trabajo 13

---
+++ trabajo_13/app/authController.php
@@ -0,0 +1,61 @@
+<?php
+
+    if(isset($_POST['access'])){
+        switch ($_POST['access']) {
+            case 'access':
+
+                $authController = new AuthController();
+                $email = $_POST['correo'];
+                $password = $_POST['contrasenna'];
+
+                $authController->authenticate($email, $password);
+
+                break;
+            
+            default:
+                # code...
+                break;
+        }
+    }
+
+    class AuthController{
+        public function authenticate($email = null, $password = null){
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/login',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array('email' => $email,'password' => $password),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            
+            if(isset($response)){
+                $response = json_decode($response, true);
+
+                $data = $response->data;
+
+                $id = $data->id;
+                echo $id;
+
+                session_start();
+
+                $_SESSION['data'] = $data;
+                $_SESSION['id'] = $id;
+
+                header("Location: ../home.php");
+            }
+
+        }
+    }
+
+?>
---
+++ trabajo_13/details.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>
+		Home
+	</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" >
+  </head>
+</head>
+<body>
+ 	
+	<div class=""> 
+	 	
+	 	<div class="container-fluid">
+	 		<div class="row">
+	 			<div class="col-2 p-0 m-0 d-none d-md-block">
+	 				<div class="d-flex flex-column min-vh-100 flex-shrink-0 p-3 text-white bg-dark">
+					    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+					      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+					      <span class="fs-4">Sidebar</span>
+					    </a>
+					    <hr>
+					    <ul class="nav nav-pills  flex-column mb-auto">
+					      <li class="nav-item">
+					        <a href="#" class="nav-link active" aria-current="page">
+					          <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
+					          Home
+					        </a>
+					      </li>
+					      <li>
+					        <a href="#" class="nav-link text-white">
+					          <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
+					          Dashboard
+					        </a>
+					      </li>
+					      <li>
+					        <a href="#" class="nav-link text-white">
+					          <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
+					          Orders
+					        </a>
+					      </li>
+					      <li>
+					        <a href="#" class="nav-link text-white">
+					          <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
+					          Products
+					        </a>
+					      </li>
+					      <li>
+					        <a href="#" class="nav-link text-white">
+					          <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"></use></svg>
+					          Customers
+					        </a>
+					      </li>
+					    </ul>
+					    <hr>
+					    <div class="dropdown">
+					      <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+					        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+					        <strong>mdo</strong>
+					      </a>
+					      <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+					        <li><a class="dropdown-item" href="#">New project...</a></li>
+					        <li><a class="dropdown-item" href="#">Settings</a></li>
+					        <li><a class="dropdown-item" href="#">Profile</a></li>
+					        <li><hr class="dropdown-divider"></li>
+					        <li><a class="dropdown-item" href="#">Sign out</a></li>
+					      </ul>
+					    </div>
+					</div>
+	 			</div>
+	 			<div class="col p-0 m-0">
+	 				<nav class="navbar navbar-expand-lg bg-body-tertiary bg-dar" data-bs-theme="dark">
+					  <div class="container">
+					    <a class="navbar-brand" href="#">Navbar</a>
+					    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					      <span class="navbar-toggler-icon"></span>
+					    </button>
+
+
+					    <div class="collapse navbar-collapse" id="navbarNav">
+					      <ul class="navbar-nav">
+					        <li class="nav-item">
+					          <a class="nav-link active" aria-current="page" href="#">Home</a>
+					        </li>
+					        <li class="nav-item">
+					          <a class="nav-link" href="#">Features</a>
+					        </li>
+					        <li class="nav-item">
+					          <a class="nav-link" href="#">Pricing</a>
+					        </li>
+					        <li class="nav-item">
+					          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+					        </li>
+					      </ul>
+					    </div>
+
+					    <form class="d-flex" role="search">
+					      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+					      <button class="btn btn-outline-success" type="submit">Search</button>
+					    </form>
+
+					    <div class="dropdown">
+					      <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+					        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+					        <strong>mdo</strong>
+					      </a>
+					      <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+					        <li><a class="dropdown-item" href="#">New project...</a></li>
+					        <li><a class="dropdown-item" href="#">Settings</a></li>
+					        <li><a class="dropdown-item" href="#">Profile</a></li>
+					        <li><hr class="dropdown-divider"></li>
+					        <li><a class="dropdown-item" href="#">Sign out</a></li>
+					      </ul>
+					    </div>
+					  </div>
+					</nav>
+
+
+					<main class="container-fluid">
+                        <div class="card">
+                            <div class="card-header">
+                                Featured
+                            </div>
+                            <div class="card-body d-flex flex-column flex-md-row">
+                                <section class="flex-shrink-0">
+                                    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                                        <div class="carousel-inner">
+                                          <div class="carousel-item active">
+                                            <img class="d-block w-100" src="https://ui-avatars.com/api/?name=Karlo+Salvador&size=320" alt="First slide">
+                                          </div>
+                                          <div class="carousel-item">
+                                            <img class="d-block w-100" src="https://ui-avatars.com/api/?name=Sarlo+Salvador&size=320" alt="Second slide">
+                                          </div>
+                                          <div class="carousel-item">
+                                            <img class="d-block w-100" src="https://ui-avatars.com/api/?name=Jarlo+Salvador&size=320" alt="Third slide">
+                                          </div>
+                                        </div>
+                                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                          <span class="visually-hidden">Previous</span>
+                                        </button>
+                                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                          <span class="visually-hidden">Next</span>
+                                        </button>
+                                      </div>
+                                </section>
+                                <section class="p-3">
+                                    <h5 class="card-title">Special title treatment</h5>
+                                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ut voluptatibus cumque neque optio sapiente laborum quasi dolorum itaque, expedita error numquam qui pariatur ratione rerum nobis quia in assumenda.</p>
+                                    <a href="details.html" class="btn btn-primary">Go somewhere</a>
+                                </section>
+                            </div>
+                        </div>
+                    
+                        <div class="card">
+                            <h5 class="card-header">Historial de pedidos</h5>
+                            <div class="card-body">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>First</th>
+                                            <th>Last</th>
+                                            <th>Handle</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>2</td>
+                                            <td>3</td>
+                                            <td>4</td>
+                                        </tr>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>2</td>
+                                            <td>3</td>
+                                            <td>4</td>
+                                        </tr>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>2</td>
+                                            <td>3</td>
+                                            <td>4</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    
+                      </main>
+
+	 			</div>
+	 		</div>
+	 	</div>
+		
+
+		
+
+
+	</div>
+	
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
